3616214dcb3b408895e9cac535e250eb2fc135cf
Yauhen Habinet <yauhen_habinet@epam.com>
2025-01-27 21:10:51 +0100

    Module_5-GitHUb_Actions

--- wdio.conf.ts
+++ wdio.conf.ts
@@ -157,7 +157,17 @@ exports.config = {
 	// Test reporter for stdout.
 	// The only one supported by default is 'dot'
 	// see also: https://webdriver.io/docs/dot-reporter
-	reporters: [[Reporter, rpConfig]],
+	reporters: [
+		'spec',
+		[
+			'allure',
+			{
+				outputDir: 'allure-results',
+				disableWebdriverStepsReporting: true,
+				disableWebdriverScreenshotsReporting: true,
+			},
+		],
+	],
 
 	// Options to be passed to Mocha.
 	// See the full list at http://mochajs.org/
