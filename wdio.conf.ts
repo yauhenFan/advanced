8d945ee22d9bf2db7bbdf414d9abf1f03f27f8d0
Yauhen Habinet <yauhen_habinet@epam.com>
2025-01-28 16:51:56 +0100

    Module_5-GitHUb_Actions

--- wdio.conf.ts
+++ wdio.conf.ts
@@ -2,9 +2,9 @@ import { Reporter } from '@reportportal/agent-js-webdriverio';
 
 const rpConfig = {
 	apiKey:
-		'apiKey_gySMGjwJTKqtB_BouzG2DUHzW2iPXWBJFMwMf5B6ZmFVp0IZZhTsDZIcApdYYR3F',
-	endpoint: 'https://rp.epam.com/api/v1',
-	project: 'yauhen_habinet_personal',
+		'apiKey_pcpiQUESTKeQNzruqmudOcv4SolzhasELqHUE7wYNaU_pfvOsrfxwOuTqYkkzxy1',
+	endpoint: 'http://localhost:8080/api/v1',
+	project: 'default_personal',
 	launch: 'dashboard',
 	mode: 'DEFAULT',
 	debug: false,
