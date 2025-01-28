244d42314f043d8ddb0512109ee757d47d68755a
Yauhen Habinet <yauhen_habinet@epam.com>
2025-01-28 10:30:47 +0100

    Module_5-GitHUb_Actions

--- wdio.conf.ts
+++ wdio.conf.ts
@@ -62,7 +62,7 @@ exports.config = {
 	// and 30 processes will get spawned. The property handles how many capabilities
 	// from the same test should run tests.
 	//
-	maxInstances: 10,
+	maxInstances: 1,
 	//
 	// If you have trouble getting all important capabilities together, check out the
 	// Sauce Labs platform configurator - a great tool to configure your capabilities:
@@ -70,7 +70,7 @@ exports.config = {
 	//
 	capabilities: [
 		{
-			maxInstances: 4,
+			maxInstances: 1,
 			browserName: 'chrome',
 			'goog:chromeOptions': {
 				args: [
