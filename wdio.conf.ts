fd0d1caa69ed84bfe253eb66d4889c68614733eb
Yauhen Habinet <yauhen_habinet@epam.com>
2025-01-28 21:25:05 +0100

    Module_5-GitHUb_Actions

--- wdio.conf.ts
+++ wdio.conf.ts
@@ -41,7 +41,7 @@ exports.config = {
 	// The path of the spec files will be resolved relative from the directory of
 	// of the config file unless it's absolute.
 	//
-	specs: ['./src/tests/**/*.e2e.js'],
+	specs: ['./src/tests/*.e2e.js'],
 	// Patterns to exclude.
 	exclude: [
 		// 'path/to/excluded/files'
