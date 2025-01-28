5d8535b48e89be939233cf2866a25ff73d59b0b4
Yauhen Habinet <yauhen_habinet@epam.com>
2025-01-28 11:55:11 +0100

    Module_5-GitHUb_Actions

--- wdio.conf.ts
+++ wdio.conf.ts
@@ -3,7 +3,7 @@ import { Reporter } from '@reportportal/agent-js-webdriverio';
 const rpConfig = {
 	apiKey:
 		'apiKey_pcpiQUESTKeQNzruqmudOcv4SolzhasELqHUE7wYNaU_pfvOsrfxwOuTqYkkzxy1',
-	endpoint: 'http://localhost:8080/ui/',
+	endpoint: 'http://localhost:8080/ui',
 	project: '#default_personal',
 	launch: 'dashboard',
 	mode: 'DEFAULT',
